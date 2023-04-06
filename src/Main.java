93bcfb5a6e1ccd94675e1cc1297ef62c9e34561c
kerim4ik04 <kboloda16@gmail.com>
2023-04-07 04:33:42 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] xbckf = { 1,2,3,4,7,6,9};
+        System.out.println(xbckf[0]);
+        System.out.println(xbckf[2]);
+        System.out.println(xbckf[1]);
+        System.out.println(xbckf[3]);
+        System.out.println(Arrays.toString(xbckf));
+        xbckf [2] = 63;
+        System.out.println(xbckf[2]);
+        int [] cbckf = new int[5];
+        System.out.println(Arrays.toString(cbckf));
+        cbckf [3] = 2;
+        System.out.println(cbckf[3]);
+        System.out.println(xbckf[5]);
+        double [] rthbv = new double[9];
+        rthbv [4] = 5.65;
+        System.out.println(rthbv[4]);
+        rthbv [0] = 2.3;
+        rthbv [1] = 45.1;
+        rthbv [2] = 23.24;
+        rthbv [3] = 426.24;
+        rthbv [6] = 72.3;
+        rthbv [5] = 23.4;
+        rthbv [7] = 23.4;
+        rthbv [8] = 324.23;
+        System.out.println(Arrays.toString(rthbv));
+        rthbv = new double[5];
+        System.out.println(Arrays.toString(rthbv));
+        System.out.println("если в низу будет " + 63 + " то юсуф лох");
+        System.out.println(xbckf[2]);
+        System.out.println("хааааа юсуф лох!!!!!!");
+        double [] xfhvb = new double[4];
+        System.out.println(Arrays.toString(xfhvb));
+        System.out.println(2222);
+        xfhvb[1] =2;
+        System.out.println(xfhvb[1]);
+    }
+}
